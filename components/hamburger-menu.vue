b57cd064618eafc142bc2f2f5973d6f7719c6ab7
Jan Hettenkofer <jan@hettenkofer.net>
2021-01-02 23:37:24 +0100

    feat: mobile friendly menu

---
+++ components/hamburger-menu.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="bz-hamburger">
+        <div class="mobile-navigation-container" v-if="open">
+            <ul class="navigation p-4">
+                <li v-for="ln of links" :key="ln.target"><h3><nuxt-link :to="ln.target">{{ln.label}}</nuxt-link></h3></li>
+            </ul>
+        </div>
+        <button class="hamburger-button" @click="open = !open" :class="{open}">
+            <div class="hamburger-dash"></div>
+            <div class="hamburger-dash"></div>
+            <div class="hamburger-dash"></div>
+        </button>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import icon from '~/components/icon.vue'
+
+export default Vue.extend({
+    components: {
+        icon
+    },
+    props: {
+        links: {
+            type: Array,
+            default() { return []; },
+        }
+    },
+    data() {
+        return {
+            open: false,
+        }
+    },
+})
+</script>
+
+<style scoped>
+.bz-hamburger {
+    position: relative;
+}
+
+.hamburger-button {
+    position: absolute;
+    background-color: var(--foreground);
+    border: solid var(--line-width) var(--background);
+    @apply p-1;
+    padding-top: calc(0.25 * var(--spacing-xl));
+    padding-bottom: calc(0.25 * var(--spacing-xl));
+    height: var(--spacing-xl);
+    width: var(--spacing-xl);
+    right: 0px;
+    bottom: 0px;
+}
+
+.hamburger-button.open {
+    background-color: var(--background);
+    border-color: var(--foreground);
+}
+
+.hamburger-dash {
+    background-color: var(--background);
+    width: 100%;
+    height: calc(2 * var(--line-width));
+    margin-bottom: calc(25% - var(--line-width));
+}
+
+.hamburger-button.open .hamburger-dash {
+    background-color: var(--foreground);
+}
+
+.mobile-navigation-container {
+  position: relative;
+
+  text-align: right;
+}
+
+.mobile-navigation-container .navigation {
+  background-color: var(--background);
+  border: solid var(--line-width) var(--foreground);
+
+  @apply pb-12;
+}
+
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,43 @@
 <template>
     <div class="dark">
-      <div id="layout-content" class="flex flex-row">
-        <div id="left-column" class="container lg:max-w-sm"></div>
-        <div id="content" class="container mx-auto pb-4 pt-4">
+      <div id="layout-content" class="container mx-auto flex flex-row">
+        <div id="left-column" class="flex-none hidden xl:block w-64"></div>
+        <div id="content" class="flex-1 mx-auto p-4">
           <nuxt />
         </div>
-        <div id="navigation-container" class="container lg:max-w-sm">
+        <div id="desktop-navigation-container" class="flex-none hidden xl:block w-64">
           <ul id="navigation" class="user-nav p-4">
-            <li><h3><nuxt-link to="/">Home</nuxt-link></h3></li>
-            <li><h3><nuxt-link to="/about">What is Brotzeit?</nuxt-link></h3></li>
+            <li v-for="ln of links" :key="ln.target"><h3><nuxt-link :to="ln.target">{{ln.label}}</nuxt-link></h3></li>
           </ul>
         </div>
       </div>
+      <hamburger-menu id="hamburger" :links="links"></hamburger-menu>
+      <!-- <div id="mobile-navigation-container" class="block xl:hidden p-4">
+        <ul id="navigation" class="p-4">
+          <li v-for="ln of links" :key="ln.target"><h3><nuxt-link :to="ln.target">{{ln.label}}</nuxt-link></h3></li>
+        </ul>
+      </div> -->
     </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HamburgerMenu from '~/components/hamburger-menu.vue'
+
+export default Vue.extend({
+  components: {
+    HamburgerMenu,
+  },
+  data() {
+    return {
+      links: [
+        { label: "Home", target: "/"},
+        { label: "What is Brotzeit?", target: "/about"},
+      ]
+    }
+  }
+})
+</script>
 
 <style scoped>
 
@@ -23,5 +47,26 @@
   position: fixed;
   bottom: 0px;
 }
+
+#hamburger {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  z-index: 99999;
+}
+
+/* #mobile-navigation-container {
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  z-index: 9999;
+
+  text-align: right;
+}
+
+#mobile-navigation-container #navigation {
+  background-color: var(--background);
+  border: solid var(--line-width) var(--foreground);
+} */
 
 </style>
