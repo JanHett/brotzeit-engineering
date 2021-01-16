9c1ca7a064f2c297af1eed3396e9b0fca0898401
JanHett <JanHett@users.noreply.github.com>
2021-01-16 18:45:13 +0000

    deploy: 34eb17ed31f691294839cc6b49a549cecdf5a3bd

---
+++ _nuxt/static/1610822706/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcomponents","\u002Farticles\u002Fcoming_soon","\u002Farticles\u002Fimpromptu-barbecue","\u002Farticles\u002Fkitchen-alchemy-butter","\u002Farticles\u002Ffinding-the-positives-in-negatives"]})
